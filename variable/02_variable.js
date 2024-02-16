9061e9df46aafd29940bf7a36c7c7dceaaa509b4
AkashSingh1141144 <AkashSingh.Webdeveloper@gmail.com>
2024-02-16 16:13:51 +0530

    second commit

---
+++ variable/02_variable.js
@@ -0,0 +1,10 @@
+var greeting = 'Hello, world!';
+let user = 'John';
+let dream = 'incomplete'
+const myName = 'Akash';
+user = 'Jane';
+dream = 'Pooja';
+console.log(greeting);
+console.log(user);
+console.log(myName);
+console.log('This is a varibles:', [greeting, user, myName, dream]);
