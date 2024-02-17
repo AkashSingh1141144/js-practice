49e993c93bcfd047b8a0619939d5dace3466a690
AkashSingh1141144 <AkashSingh.Webdeveloper@gmail.com>
2024-02-17 08:44:27 +0530

    Conversion of DataType

---
+++ DataTypes_conversion/conversion.js
@@ -0,0 +1,40 @@
+// let score = "33"
+// let score ='33abc'
+// let score = null
+// let score = undefined
+// let score = true
+let score = 'Akash singh'
+
+// const {score} = req.body
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// '33' => 33
+// '33abc' => Nan
+// true = 1; false => 0
+
+
+// let isLoggdIn = 1
+// let isLoggdIn = ""
+let isLoggdIn = 'Akash'
+ 
+let booleanIsLoggedIn = Boolean(isLoggdIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "akash" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
