9c04f8766859a7f0c0d9740658811ef7fcec8170
AkashSingh1141144 <AkashSingh.Webdeveloper@gmail.com>
2024-02-17 09:04:30 +0530

    DataType

---
+++ DataTypes_conversion/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( "Hello World" ); we are using node js, not browser 
+
+console.log(3 + 3);
+ console.log('akash'); // code readability should be high
+
+
+
+ let name = 'Akash'
+ let age = 20
+ let isLoggdIn = true
+ let state;
+ let tempreture = null
+
+//  number => 2 to power 53
+// bigint 
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+ console.log(typeof "Akash");
+ console.log(typeof age);
+ console.log(typeof null); // object
+ console.log(typeof undefined); // undefined
