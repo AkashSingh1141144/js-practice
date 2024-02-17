b65d6b926c06909e0b76d16a0ec654b461c38687
AkashSingh1141144 <AkashSingh.Webdeveloper@gmail.com>
2024-02-17 17:27:36 +0530

    Postfix&Prefix_DType

---
+++ DataTypes_conversion/01_postfix&prifix.js
@@ -0,0 +1,18 @@
+let a = 5;
+console.log("the value of the postfix is: ", a++);
+
+let b = 5;
+console.log("the value of the prefix is: ", ++b);
+
+
+// ** 2
+
+let counter = 3;
+counter++;     // 4 the incremented value
+console.log(counter); // 4 incremented value has been returned
+
+
+
+let counter2 = 3;
+++counter2;   // 3 the original value
+console.log(counter2); // 4 value had now been incrimented and returns the new value
---
+++ DataTypes_conversion/postfixAndPrifix.js
@@ -0,0 +1,21 @@
+/* In JavaScript, the terms "postfix" and "prefix " are typically associated with increment and decrement operatore. ++ and --*/
+
+// Prefix
+let a = 5;
+let b = ++a; // Increment a before using a value
+
+console.log(a);
+console.log(b);
+
+
+// Postfix 
+
+let a1 = 5;
+let b1 = a1++; // Increment a after using its values
+
+console.log(a1);
+console.log(b1);
+
+
+/* in the prefix form, the value of the variable is incremented or decremented before the value is used in the expression. In the postfix form, the value of the variable is used in the expression first, and then it is incremented or decremented.
+*/
