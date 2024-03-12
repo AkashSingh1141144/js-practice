f9557ae18d3c11fa565ad97b0e87db9fd4bffa17
AkashSingh1141144 <AkashSingh.Webdeveloper@gmail.com>
2024-03-13 03:10:46 +0530

    pw skills array

---
+++ PW-SKILL-JS/Arrays/one.js
@@ -0,0 +1,10 @@
+/*  What is array in js
+JavaScript arrays are ordered collections of values called elements. They are used to store multiple values in a single variable. Elements in an array can be accessed using their index (location in the array). JavaScript arrays are dynamic in nature, which means their size can vary. They can also be nested, meaning they can contain other arrays.
+*/
+const array = [1, 2, 3, 4, 5];
+console.log(array[0]);
+
+let b = new Array('abc', 'def', 'ghi')
+console.log(b);
+
+
